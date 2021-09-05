962a514fecccb66cbf4068148bdaffa927bb0ec9
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-09-05 04:21:12 -0700

    Sucessfully Added

--- EmployeePayRoll/js/EmployeePayrollData.js
+++ EmployeePayRoll/js/EmployeePayrollData.js
@@ -60,8 +60,14 @@ class PersonInfo {
     }
 
     set start_date ( start_date ) {
-        this._start_date = start_date;
-    } 
+        let now = new Date();
+        now = Date.parse(now);
+        if ( start_date > now ) {
+            throw 'Start Date is Future date!';
+        } else {
+            this._start_date = start_date;
+        }
+    }
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -69,6 +75,6 @@ class PersonInfo {
                         this.start_date.toLocaleDateString("en-US", options);
         return 'Name = ' + this.name + ", Gender = " + this.gender + ", ProfilePic = " +this.profilePic
                     + ", Department = " + this.department + ", Salary = " + this.salary +
-                        ", StartDate = " + empDate + ", Note = " +this.note;
+                        ", StartDate = " + this.start_date + ", Note = " +this.note;
     }
 }
